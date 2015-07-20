cf4eb55d8894d67775735e7356722a7ff6274d80
SmartBros <arunsenthilin@gmail.com>
2015-07-20 21:34:28 +0530

    Create _.each_example.js

---
+++ _.each_example.js
@@ -0,0 +1,26 @@
+var utilityArray1 = [{
+ "Name" : "BOB",
+ "age" : 27
+}, {
+ "Name" : "Kevin",
+ "age" : 22
+}];
+
+var utilityArray2 = [{
+ "Name" : "BOB",
+ "age" : 27
+}, {
+ "Name" : "Kevin",
+ "age" : 22
+}];
+
+_.each(utilityArray1, function(Obj1) {
+    console.log("==== Name in utilityArray1 ===\n " + Obj1.Name);
+    console.log("==== Age in utilityArray1 ===\n " + Obj1["age"]);
+    _.each(utilityArray2, function(Obj2) {
+        if (Obj2["age"] === Obj1.age) {
+            console.log("==== Name in utilityArray2===\n " + Obj2.Name);
+            console.log("==== Age in utilityArray2===\n " + Obj2["age"]);
+        }
+    });
+});
